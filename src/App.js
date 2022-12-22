98d9aa980f9eb0ba852c1c352099942453acd473
Nazmush Shakib <shakibgithub944>
2022-12-23 00:23:31 +0600

    details fatched

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Components/Home';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import UserDetails from './Components/UserDetails';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home></Home>
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/details/:id',
+        element: <UserDetails></UserDetails>,
+        loader: async ({ params }) => {
+          return fetch(`https://602e7c2c4410730017c50b9d.mockapi.io/users/${params.id}`);
+        },
+      }
+    ]
   }
 ])
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,13 +3,12 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Spinner from 'react-bootstrap/Spinner';
-import Form from 'react-bootstrap/Form';
 import axios from 'axios';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 const Home = () => {
     const [users, setUsers] = useState([]);
-    const [user, setUser] = useState({});
+    const [empty, setEmpty] = useState(true)
     const [loading, setLoading] = useState(false)
 
     useEffect(() => {
@@ -21,19 +20,6 @@ const Home = () => {
             })
     }, [])
 
-    // const userDetails = (id) => {
-    //     setLoading(true)
-    //     fetch('https://602e7c2c4410730017c50b9d.mockapi.io/users')
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             const result = data.filter(user => user.id === id);
-    //             // console.log(result[0]);
-    //             setUser(result[0])
-    //             setLoading(false)
-
-    //         })
-    // }
-
     if (loading) {
         return (
             <div className='d-flex align-items-center justify-content-center my-5'>
@@ -42,34 +28,45 @@ const Home = () => {
         )
     }
 
-    console.log(user);
-
     return (
         <div>
             <Container className=''>
                 <Row className='my-5'>
                     <Col className='p-50'>
-                        <h3 className='bg-info text-white text-center py-2'>User List</h3>
+                        <h3 className='bg-info border rounded text-white text-center py-2'>User List</h3>
                         {
-                            users.map((user) => <div
+                            users.map((user) => <Link
                                 key={user.id}
-                                className='border rounded my-2'
-                                // onClick={() => userDetails(user.id)}
+                                to={`/details/${user.id}`}
+                                className='text-decoration-none text-black'
+                                onClick={() => setEmpty(false)}
                             >
-                                <div className="d-flex align-items-center p-2">
-                                    <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" className='' style={{
-                                        width: '40px',
-                                        height: '40px'
-                                    }} alt="" />
-                                    <p className='mb-0 ms-2 fw-bold'>{user.profile.username}</p>
+                                <div
+
+                                    className='border rounded my-2'
+                                // onClick={() => userDetails(user.id)}
+                                >
+                                    <div className="d-flex align-items-center p-2">
+                                        <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" className='' style={{
+                                            width: '40px',
+                                            height: '40px'
+                                        }} alt="" />
+                                        <p className='mb-0 ms-2 fw-bold'>{user.profile.username}</p>
+                                    </div>
                                 </div>
-                            </div>)
+                            </Link>)
                         }
 
                     </Col>
                     <Col className='p-50'>
-                        <h3 className='bg-info text-white text-center py-2'>User Details</h3>
-                        <Outlet></Outlet>
+                        <h3 className='bg-info border rounded text-white text-center py-2'>User Details</h3>
+                        {
+                            empty ? <div className='d-flex align-items-center justify-content-center h-100'>
+                                <h2 className='text-muted'>Please select a user</h2>
+                            </div>
+                                :
+                                <Outlet></Outlet>
+                        }
                     </Col>
 
                 </Row>
